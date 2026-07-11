324a1a785423de21cf324d8c42ff9a7ca0950860
takalocash <takalocash@gmail.com>
2026-07-11 07:00:20 +0300

    Update and rename config.js to .nojekyll

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js - Fampifandraisana ny TakaloCash amin'ny Supabase
-const SUPABASE_URL = "https://nyeopreahmajoisartoz.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_xR8d2_nnrLURdRLefd4i9Q_mSQ7CYJ9";
-
-// Manomboka ny client Supabase
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
